a9ede66e04ab8c6572e1e8fe569a8620e7d37cf7
Xl.W <shawlix+git@gmail.com>
2017-07-29 03:27:40 +0800

    drivers: media: add actuator change
    Change-Id: Ibf482d308992c7a047ce097464bd40f6878500b6

--- drivers/media/video/msm/actuators/msm_actuator.c
+++ drivers/media/video/msm/actuators/msm_actuator.c
@@ -236,7 +236,9 @@ static int32_t msm_actuator_move_focus(
 {
 	int32_t rc = 0;
 	int8_t sign_dir = move_params->sign_dir;
+#ifndef CONFIG_IMX175
 	uint16_t step_boundary = 0;
+#endif
 	uint16_t target_step_pos = 0;
 	uint16_t target_lens_pos = 0;
 	int16_t dest_step_pos = move_params->dest_step_pos;
@@ -286,12 +288,13 @@ static int32_t msm_actuator_move_focus(
 		a_ctrl->curr_step_pos, dest_step_pos, curr_lens_pos);
 
 	while (a_ctrl->curr_step_pos != dest_step_pos) {
+#ifndef CONFIG_IMX175
 		step_boundary =
 			a_ctrl->region_params[a_ctrl->curr_region_index].
 			step_bound[dir];
 		if ((dest_step_pos * sign_dir) <=
 			(step_boundary * sign_dir)) {
-
+#endif
 			target_step_pos = dest_step_pos;
 			target_lens_pos =
 				a_ctrl->step_position_table[target_step_pos];
@@ -308,7 +311,7 @@ static int32_t msm_actuator_move_focus(
 				return rc;
 			}
 			curr_lens_pos = target_lens_pos;
-
+#ifndef CONFIG_IMX175
 		} else {
 			target_step_pos = step_boundary;
 			target_lens_pos =
@@ -329,6 +332,7 @@ static int32_t msm_actuator_move_focus(
 
 			a_ctrl->curr_region_index += sign_dir;
 		}
+#endif
 		a_ctrl->curr_step_pos = target_step_pos;
 	}
 
